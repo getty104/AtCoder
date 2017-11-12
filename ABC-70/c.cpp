1a42402f1b39949e09824903e4a9593872eb0002
getty104 <hayabusatoshihumi@gmail.com>
2017-11-12 21:56:47 +0900

    update

---
+++ ABC-70/c.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef vector<ll> Vector;
+typedef vector<vector<ll>> DVector;
+
+#define fi          first
+#define se          second
+#define pb          push_back
+#define INF         INT_MAX/3
+#define bcnt        __builtin_popcount
+#define all(x)      (x).begin(),(x).end()
+#define dbg(x)      cout<<#x"="<<x<<endl
+#define ub(x,y)     upper_bound(all(x),y)-x.begin()
+#define lb(x,y)     lower_bound(all(x),y)-x.begin()
+#define uni(x)      x.erase(unique(all(x)),x.end())
+#define rep(i,n)    repl(i,0,n-1)
+#define repl(i,a,b) for(ll i=(ll)(a);i<=(ll)(b);i++)
+#define mmax(x,y)   (x>y?x:y)
+#define mmin(x,y)   (x<y?x:y)
+#define maxch(x,y)  x=mmax(x,y)
+#define minch(x,y)  x=mmin(x,y)
+#define exist(x,y)  (find(all(x),y)!=x.end())
+#define each(itr,v) for(auto itr:v)
+#define usort(x)    sort(all(x))
+#define dsort(x)    sort(all(x),greater<int>())
+#define mkp(x,y)    make_pair(x,y)
+ll gcd(ll a,ll b){
+  if(b>a)gcd(b,a);
+  ll r = a%b;
+  if(r==0)return b;
+  return gcd(b,r);
+}
+
+int main(){
+  cin.sync_with_stdio(false);
+  int n;
+  cin >> n;
+  Vector t(n);
+  rep(i,n)cin >> t[i];
+  rep(i,n-1) t[i+1] = t[i]*(t[i+1]/gcd(t[i],t[i+1]));
+  cout << t[n-1]<< endl;
+}
