731d014a6320e2d141080afef8c558868e4ad468
getty104 <hayabusatoshihumi@gmail.com>
2017-11-19 18:28:52 +0900

    update

---
+++ ABC-79/c.rb
@@ -0,0 +1,37 @@
+require 'prime'
+require 'set'
+require 'tsort'
+include Math
+ALP = ('a'..'z').to_a
+INF = 0xffffffffffffffff
+def max(a,b);              a > b ? a : b                              end
+def min(a,b);              a < b ?  a : b                             end
+def swap(a,b);             a, b = b, a                                end
+def gif;                   gets.to_i                                  end
+def gff;                   gets.to_f                                  end
+def gsf;                   gets.chomp                                 end
+def gi;                    gets.split.map(&:to_i)                     end
+def gf;                    gets.split.map(&:to_f)                     end
+def gs;                    gets.chomp.split.map(&:to_s)               end
+def gc;                    gets.chomp.split('')                       end
+def pr(num);               num.prime_division                         end
+def pr?(num);              Prime.prime?(num)                          end
+def digit(num);            num.to_s.length                            end
+def array(s,ini=nil);      Array.new(s){ini}                          end
+def darray(s1,s2,ini=nil); Array.new(s1){Array.new(s2){ini}}          end
+def rep(num);              num.times{|i|yield(i)}                     end
+def repl(st,en,n=1);       st.step(en,n){|i|yield(i)}                 end
+
+op = ["+","-"]
+n = gc
+stack = []
+op.each do |op1|
+  op.each do |op2|
+    op.each do |op3|
+      str = "#{n[0]}#{op1}#{n[1]}#{op2}#{n[2]}#{op3}#{n[3]}"
+      ans = (eval str)
+      stack << str+"=7" if ans == 7
+    end
+  end
+end
+puts stack.first
