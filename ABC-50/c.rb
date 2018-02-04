047ea740f143109bfc044d9af1163b4360c210b2
getty104 <hayabusatoshihumi@gmail.com>
2018-02-04 14:41:31 +0900

    updat

--- ABC-50/c.rb
+++ ABC-50/c.rb
@@ -1,54 +1,68 @@
 require 'prime'
+require 'set'
+require 'tsort'
 include Math
-def max(a,b);                           a > b ? a : b                             end
-def min(a,b);                           a < b ?  a : b                            end
-def swap(a,b);                          a, b = b, a                               end
-def gif;                                gets.to_i                                 end
-def gi;                                 gets.split.map(&:to_i)                    end
-def gs;                                 gets.chomp                                end
-def gc;                                 gets.chomp.split('')                      end
-def pr(num);                            num.prime_division                        end
-def putall(obj);                        obj.each{|o| puts(o)}                     end
-def array(size,init = nil);             Array.new(size){init}                     end
-def darray(size1, size2, init = nil);   Array.new(size1){Array.new(size2){init}}  end
-def rep(num);                           num.times do|i|yield(i) end               end
-  def repl(st,en);                      st.upto en-1 do|i|yield(i) end            end
+ALP = ('a'..'z').to_a
+INF = 0xffffffffffffffff
+def max(a,b);              a > b ? a : b                              end
+def min(a,b);              a < b ?  a : b                             end
+def swap(a,b);             a, b = b, a                                end
+def gif;                   gets.to_i                                  end
+def gff;                   gets.to_f                                  end
+def gsf;                   gets.chomp                                 end
+def gi;                    gets.split.map(&:to_i)                     end
+def gf;                    gets.split.map(&:to_f)                     end
+def gs;                    gets.chomp.split.map(&:to_s)               end
+def gc;                    gets.chomp.split('')                       end
+def pr(num);               num.prime_division                         end
+def pr?(num);              Prime.prime?(num)                          end
+def digit(num);            num.to_s.length                            end
+def array(s,ini=nil);      Array.new(s){ini}                          end
+def darray(s1,s2,ini=nil); Array.new(s1){Array.new(s2){ini}}          end
+def rep(num);              num.times{|i|yield(i)}                     end
+def repl(st,en,n=1);       st.step(en,n){|i|yield(i)}                 end
 
-  def culcx x
-  	if x > 0
-  		"R" * x
-  	elsif x < 0
-  		"L" * -x
-  	end
+n = gif
+a = gi
+b = []
+if n % 2 == 0
+  repl n-1, 1, -2 do |variable|
+    b << variable
   end
 
-  def culcy y
-  	if y > 0
-  		"U" * y
-  	elsif y < 0
-  		"D" * -y
-  	end
+  repl 1, n-1,2 do |variable|
+    b << variable
+  end
+else
+  repl n-1, 0, -2 do |variable|
+    b << variable
   end
 
-  sx,sy,tx,ty = gi
-  str = ""
- 	x = tx-sx
- 	y = ty-sx
- 	if x > 0 && y >0
- 	str += culcy y
- 	str += culcx x
- 	str += culcy -y
- 	str += culcx -x
- 	str += culcx -1
- 	str += culcy y+1
- 	str += culcx x+1
- 	str += culcy -1
- 	str += culcx 1
- 	str += culcy -(y+1)
- 	str += culcx -(x+1)
- 	str += culcy 1
- 	puts str
- elsif x > 0 && y < 0
- elsif x < 0 && y < 0
- elsif x < 0 && y > 0
- end
+  repl 2, n-1, 2 do |variable|
+    b << variable
+  end
+end
+
+
+aa = Hash.new
+bb = Hash.new
+a.each do |ta|
+  aa[ta] ||= 0
+  aa[ta] += 1
+end
+
+b.each do |tb|
+  bb[tb] ||= 0
+  bb[tb] += 1
+end
+
+jd = true
+b.each do |i|
+  if bb[i] != aa[i]
+    jd = false
+    break
+  end
+end
+
+ans = jd ? 2 ** (n/2) : 0
+puts ans % (10**9 + 7)
