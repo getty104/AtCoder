b9ff2513c0ee2ccfa0fc92cebbe3c5920afb5d2b
getty104 <hayabusatoshihumi@gmail.com>
2018-02-10 12:54:26 +0900

    update

---
+++ ABC-18/b.rb
@@ -0,0 +1,41 @@
+require 'prime'
+require 'set'
+require 'tsort'
+include Math
+ALP = ('a'..'z').to_a
+INF = 0xffffffffffffffff
+def max(a,b);              a > b ? a : b                              end
+def min(a,b);              a < b ?  a : b                             end
+def swap(a,b);             a, b = b, a                                end
+def gif;                   gets.to_i                                  end
+def gff;                   gets.to_f                                  end
+def gsf;                   gets.chomp                                 end
+def gi;                    gets.split.map(&:to_i)                     end
+def gf;                    gets.split.map(&:to_f)                     end
+def gs;                    gets.chomp.split.map(&:to_s)               end
+def gc;                    gets.chomp.split('')                       end
+def pr(num);               num.prime_division                         end
+def pr?(num);              Prime.prime?(num)                          end
+def digit(num);            num.to_s.length                            end
+def array(s,ini=nil);      Array.new(s){ini}                          end
+def darray(s1,s2,ini=nil); Array.new(s1){Array.new(s2){ini}}          end
+def rep(num);              num.times{|i|yield(i)}                     end
+def repl(st,en,n=1);       st.step(en,n){|i|yield(i)}                 end
+
+str = gc
+n = gif
+rep n do
+  l,r = gi
+  l-=1
+  r-=1
+  t1 = str.slice(0..l-1)
+  t2 = str.slice(r+1..(str.size-1))
+  t3 = str.slice(l..r)
+  t3.reverse!
+  ans = []
+  ans += t1 if l > 0
+  ans += t3
+  ans += t2 if r < str.size-1
+  str = ans
+end
+puts str.join('')
