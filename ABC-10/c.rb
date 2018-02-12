69b7539054cde1981d2ee71c51f788c5ec3e54df
getty104 <hayabusatoshihumi@gmail.com>
2018-02-12 10:46:18 +0900

    update

---
+++ ABC-10/c.rb
@@ -0,0 +1,37 @@
+require 'prime'
+require 'set'
+require 'tsort'
+include Math
+ALP = ('a'..'z').to_a
+INF = 0xffffffffffffffff
+def max(a,b);              a > b ? a : b                              end
+def min(a,b);              a < b ?  a : b                             end
+def swap(a,b);             a, b = b, a                                end
+def gif;                   gets.to_i                                  end
+def gff;                   gets.to_f                                  end
+def gsf;                   gets.chomp                                 end
+def gi;                    gets.split.map(&:to_i)                     end
+def gf;                    gets.split.map(&:to_f)                     end
+def gs;                    gets.chomp.split.map(&:to_s)               end
+def gc;                    gets.chomp.split('')                       end
+def pr(num);               num.prime_division                         end
+def pr?(num);              Prime.prime?(num)                          end
+def digit(num);            num.to_s.length                            end
+def array(s,ini=nil);      Array.new(s){ini}                          end
+def darray(s1,s2,ini=nil); Array.new(s1){Array.new(s2){ini}}          end
+def rep(num);              num.times{|i|yield(i)}                     end
+def repl(st,en,n=1);       st.step(en,n){|i|yield(i)}                 end
+
+txa,tya,txb,tyb,T,V = gi
+n = gif
+jd = false
+L = T * V
+rep n do |i|
+x,y = gi
+la = sqrt((x - txa) ** 2 + (y - tya) ** 2)
+lb = sqrt((x - txb) ** 2 + (y - tyb) ** 2)
+l = la + lb
+jd = true if L >= l
+end
+
+puts jd ? "YES" : "NO"
