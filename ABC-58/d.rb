87d6619c84cd2b85a3b913298a4551ef25622767
getty104 <hayabusatoshihumi@gmail.com>
2018-02-27 00:00:32 +0900

    upadte

--- ABC-58/d.rb
+++ ABC-58/d.rb
@@ -1,37 +1,37 @@
 require 'prime'
+require 'set'
+require 'tsort'
 include Math
-def max(a,b);                    a > b ? a : b                            end
-def min(a,b);                    a < b ?  a : b                           end
-def swap(a,b);                   a, b = b, a                              end
-def gif;                         gets.to_i                                end
-def gsf;                         gets.chomp                               end
-def gi;                          gets.split.map(&:to_i)                   end
-def gs;                          gets.chomp.split.map(&:to_s)             end
-def gc;                          gets.chomp.split('')                     end
-def pr(num);                     num.prime_division                       end
-def putall(obj);                 obj.each{|o| puts(o)}                    end
-def array(size,init=nil);        Array.new(size){init}                    end
-def darray(size1,size2,init=nil);Array.new(size1){Array.new(size2){init}} end
-def rep(num);                    num.times {|i|yield(i)}                  end
-def repl(st,en);                 st.upto(en) {|i|yield(i)}                end
-
+ALP = ('a'..'z').to_a
+INF = 0xffffffffffffffff
+def max(a,b);              a > b ? a : b                              end
+def min(a,b);              a < b ?  a : b                             end
+def swap(a,b);             a, b = b, a                                end
+def gif;                   gets.to_i                                  end
+def gff;                   gets.to_f                                  end
+def gsf;                   gets.chomp                                 end
+def gi;                    gets.split.map(&:to_i)                     end
+def gf;                    gets.split.map(&:to_f)                     end
+def gs;                    gets.chomp.split.map(&:to_s)               end
+def gc;                    gets.chomp.split('')                       end
+def pr(num);               num.prime_division                         end
+def pr?(num);              Prime.prime?(num)                          end
+def digit(num);            num.to_s.length                            end
+def array(s,ini=nil);      Array.new(s){ini}                          end
+def darray(s1,s2,ini=nil); Array.new(s1){Array.new(s2){ini}}          end
+def rep(num);              num.times{|i|yield(i)}                     end
+def repl(st,en,n=1);       st.step(en,n){|i|yield(i)}                 end
 
 n,m = gi
 x = gi
 y = gi
-hx = []
-hy = []
-rep n-1 do |i|
-	hx << x[i+1]-x[i]
-end
-rep m-1 do |i|
-	hy << y[i+1]-y[i]
-end
-s = 0
-rep n-1 do |i|
-	rep m-1 do |j|
-		s+= hx[i]*hy[j]*(n-1-i)*(m-1-j) % ( (10 ** 9)+7 )
-	end
-end
 
-puts  s
+xx = 0
+rep n do |i|
+  xx += i*x[i] - (n - 1 - i)*x[i]
+end
+yy = 0
+rep m do |i|
+  yy += i*y[i] - (m - 1 - i)*y[i]
+end
+puts xx*yy % (10**9+7)
