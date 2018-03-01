3324d0acc60b0d7d22234415fdd37c0ec8e0f071
getty104 <hayabusatoshihumi@gmail.com>
2018-03-01 18:53:43 +0900

    update

---
+++ ABC-52/d.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll,ll> P;
+typedef pair<ll,P>  PP;
+typedef vector<ll> Vector;
+typedef vector<Vector> DVector;
+typedef priority_queue<PP, vector<PP>, greater<PP>> PPQueue;
+
+#define fi          first
+#define se          second
+#define sfi         se.fi
+#define sse         se.se
+#define pb          push_back
+#define INF         INT_MAX
+#define MOD         1000000007
+#define bcnt        __builtin_popcount
+#define all(x)      (x).begin(),(x).end()
+#define uni(x)      x.erase(unique(all(x)),x.end())
+#define ub(x,y)     (upper_bound(all(x),y)-x.begin())
+#define lb(x,y)     (lower_bound(all(x),y)-x.begin())
+#define rep(i,n)    repl(i,0,n-1)
+#define usort(x)    sort(all(x))
+#define dsort(x)    sort(all(x),greater<ll>())
+#define mkp(x,y)    make_pair(x,y)
+#define mmax(x,y)   (x>y?x:y)
+#define mmin(x,y)   (x<y?x:y)
+#define maxch(x,y)  x=mmax(x,y)
+#define minch(x,y)  x=mmin(x,y)
+#define exist(x,y)  (find(all(x),y)!=x.end())
+#define each(itr,v) for(auto itr:v)
+#define repl(i,a,b) for(ll i=(ll)(a);i<=(ll)(b);i++)
+
+int main(){
+  cin.sync_with_stdio(false);
+  ll n, a, b;
+  cin >> n >> a >> b;
+  Vector x(n);
+  rep(i,n)cin >> x[i];
+  ll sum = 0;
+  rep(i,n-1){
+    ll ta = (x[i+1] - x[i])*a;
+    sum += ta >= b ? b : ta;
+  }
+  cout << sum << endl;
+}
