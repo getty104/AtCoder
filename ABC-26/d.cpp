ea6b8104a0d72d5ad56d7819b74e25738b597a5e
getty104 <hayabusatoshihumi@gmail.com>
2018-03-25 00:12:52 +0900

    update

---
+++ ABC-26/d.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll,ll> P;
+typedef vector<ll> Vec;
+typedef vector<Vec> DVec;
+typedef priority_queue<P, vector<P>, greater<P>> PQ;
+
+#define fi             first
+#define se             second
+#define pb             push_back
+#define INF            LLONG_MAX/3
+#define MOD            1000000007
+#define bcnt           __builtin_popcount
+#define all(x)         (x).begin(),(x).end()
+#define uni(x)         x.erase(unique(all(x)),x.end())
+#define ub(x,y)        (upper_bound(all(x),y)-x.begin())
+#define lb(x,y)        (lower_bound(all(x),y)-x.begin())
+#define ssort(x)       sort(all(x))
+#define rep(i,n)       repl(i,0,n-1)
+#define mkp(x,y)       make_pair(x,y)
+#define mmax(x,y)      (x>y?x:y)
+#define mmin(x,y)      (x<y?x:y)
+#define maxch(x,y)     x=mmax(x,y)
+#define minch(x,y)     x=mmin(x,y)
+#define exist(x,y)     (find(all(x),y)!=x.end())
+#define each(itr,v)    for(auto itr:v)
+#define repl(i,a,b)    for(ll i=(ll)(a);i<=(ll)(b);i++)
+#define dvec(n1,n2,i)  DVec(n1,Vec(n2,i))
+double a,b,c;
+
+double f(double t){
+  return a*t + b * sin(c*t*M_PI);
+}
+
+int main(){
+  cin.sync_with_stdio(false);
+  cin >> a >> b >> c;
+  double le = 0;
+  double ri = 200;
+  while(ri-le>10e-12){
+    double mid = (le+ri)/2.0;
+    if(f(mid) < 100)le = mid;
+    else ri = mid;
+  }
+  cout << setprecision(16) << le << endl;
+
+  return 0;
+}
