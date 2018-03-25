997d1a95f2b8e40dd41ae30fe90c4358011e8d59
getty104 <hayabusatoshihumi@gmail.com>
2018-03-25 22:37:42 +0900

    update

---
+++ ABC-92/b.rb
@@ -0,0 +1,41 @@
+require 'prime'
+require 'set'
+require 'tsort'
+include Math
+ALP = ('a'..'z').to_a
+INF = 0xffffffffffffffff
+def max(a,b);              a > b ? a : b                              end
+def min(a,b);              a < b ?  a : b                             end
+def swap(a,b);             a, b = b, a                                end
+def gif;                   gets.to_i                                  end
+def gff;                   gets.to_f                                  end
+def gsf;                   gets.chomp                                 end
+def gi;                    gets.split.map(&:to_i)                     end
+def gf;                    gets.split.map(&:to_f)                     end
+def gs;                    gets.chomp.split.map(&:to_s)               end
+def gc;                    gets.chomp.split('')                       end
+def pr(num);               num.prime_division                         end
+def pr?(num);              Prime.prime?(num)                          end
+def digit(num);            num.to_s.length                            end
+def array(s,ini=nil);      Array.new(s){ini}                          end
+def darray(s1,s2,ini=nil); Array.new(s1){Array.new(s2){ini}}          end
+def rep(num);              num.times{|i|yield(i)}                     end
+def repl(st,en,n=1);       st.step(en,n){|i|yield(i)}                 end
+
+n = gif
+d,x = gi
+a = []
+rep n do
+  a << gif
+end
+
+ans = x
+
+a.each do |aa|
+  i = 0
+  while aa*i+1 <= d
+    ans += 1
+    i+=1
+  end
+end
+puts ans
---
+++ ABC-92/c.rb
@@ -0,0 +1,43 @@
+require 'prime'
+require 'set'
+require 'tsort'
+include Math
+ALP = ('a'..'z').to_a
+INF = 0xffffffffffffffff
+def max(a,b);              a > b ? a : b                              end
+def min(a,b);              a < b ?  a : b                             end
+def swap(a,b);             a, b = b, a                                end
+def gif;                   gets.to_i                                  end
+def gff;                   gets.to_f                                  end
+def gsf;                   gets.chomp                                 end
+def gi;                    gets.split.map(&:to_i)                     end
+def gf;                    gets.split.map(&:to_f)                     end
+def gs;                    gets.chomp.split.map(&:to_s)               end
+def gc;                    gets.chomp.split('')                       end
+def pr(num);               num.prime_division                         end
+def pr?(num);              Prime.prime?(num)                          end
+def digit(num);            num.to_s.length                            end
+def array(s,ini=nil);      Array.new(s){ini}                          end
+def darray(s1,s2,ini=nil); Array.new(s1){Array.new(s2){ini}}          end
+def rep(num);              num.times{|i|yield(i)}                     end
+def repl(st,en,n=1);       st.step(en,n){|i|yield(i)}                 end
+
+n = gif
+a = gi
+cur = 0
+sum = 0
+rep n do |i|
+  sum += (a[i] - cur).abs
+  cur = a[i]
+end
+
+cur = 0
+rep n do |i|
+  tsum = sum
+  tsum -= (a[i] - cur).abs
+  tsum -= (a[i+1]-a[i]).abs if i < n-1
+  tsum += (a[i+1] - cur).abs if i < n-1
+  cur = a[i]
+  add = i < n-1 ? a[-1] : a[-2]
+  puts tsum + add.abs
+end
