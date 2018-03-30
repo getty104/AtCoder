08bb6c956395ff30e7276bc0643717fb57e68941
getty104 <hayabusatoshihumi@gmail.com>
2018-03-30 11:26:49 +0900

    update

---
+++ ARC-39/a.rb
@@ -0,0 +1,45 @@
+require 'prime'
+require 'set'
+require 'tsort'
+include Math
+ALP = ('a'..'z').to_a
+INF = 0xffffffffffffffff
+def max(a,b);              a > b ? a : b                              end
+def min(a,b);              a < b ?  a : b                             end
+def swap(a,b);             a, b = b, a                                end
+def gif;                   gets.to_i                                  end
+def gff;                   gets.to_f                                  end
+def gsf;                   gets.chomp                                 end
+def gi;                    gets.split.map(&:to_i)                     end
+def gf;                    gets.split.map(&:to_f)                     end
+def gs;                    gets.chomp.split.map(&:to_s)               end
+def gc;                    gets.chomp.split('')                       end
+def pr(num);               num.prime_division                         end
+def pr?(num);              Prime.prime?(num)                          end
+def digit(num);            num.to_s.length                            end
+def array(s,ini=nil);      Array.new(s){ini}                          end
+def darray(s1,s2,ini=nil); Array.new(s1){Array.new(s2){ini}}          end
+def rep(num);              num.times{|i|yield(i)}                     end
+def repl(st,en,n=1);       st.step(en,n){|i|yield(i)}                 end
+
+a,b = gs
+aa = a.dup
+bb = b.dup
+rep aa.size do |i|
+  if aa[i] != '9'
+    aa[i] = '9'
+    break
+  end
+end
+
+if bb[0] != '1'
+    bb[0] = '1'
+else
+repl 1, bb.size-1 do |i|
+    if bb[i] != '0'
+        bb[i] = '0'
+        break
+    end
+end
+end
+puts max aa.to_i-b.to_i,a.to_i-bb.to_i
