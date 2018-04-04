56b60ae51dee6364d9422bf5169c8210b1c5b7ef
getty104 <hayabusatoshihumi@gmail.com>
2018-04-04 11:31:15 +0900

    update

---
+++ ARC-23/a.rb
@@ -0,0 +1,38 @@
+require 'prime'
+require 'set'
+require 'tsort'
+require 'date'
+include Math
+ALP = ('a'..'z').to_a
+INF = 0xffffffffffffffff
+def max(a,b);              a > b ? a : b                              end
+def min(a,b);              a < b ?  a : b                             end
+def swap(a,b);             a, b = b, a                                end
+def gif;                   gets.to_i                                  end
+def gff;                   gets.to_f                                  end
+def gsf;                   gets.chomp                                 end
+def gi;                    gets.split.map(&:to_i)                     end
+def gf;                    gets.split.map(&:to_f)                     end
+def gs;                    gets.chomp.split.map(&:to_s)               end
+def gc;                    gets.chomp.split('')                       end
+def pr(num);               num.prime_division                         end
+def pr?(num);              Prime.prime?(num)                          end
+def digit(num);            num.to_s.length                            end
+def array(s,ini=nil);      Array.new(s){ini}                          end
+def darray(s1,s2,ini=nil); Array.new(s1){Array.new(s2){ini}}          end
+def rep(num);              num.times{|i|yield(i)}                     end
+def repl(st,en,n=1);       st.step(en,n){|i|yield(i)}                 end
+
+y = gff
+m = gff
+d = gff
+
+def date y, m, d
+  if m <= 2
+    y -= 1
+    m += 12
+  end
+  return (365*y + (y/4).floor - (y/100).floor + (y/400).floor + (306*(m+1)/10).floor + d - 429).to_i
+end
+
+puts date(2014,5,17) - date(y,m,d)
