42125d9ee63ecaccfa80076aca9100e4f9815219
getty104 <hayabusatoshihumi@gmail.com>
2018-04-07 12:55:59 +0900

    a.rb

---
+++ ARC-06/a.rb
@@ -0,0 +1,41 @@
+require 'prime'
+require 'set'
+require 'tsort'
+include Math
+ALP = ('a'..'z').to_a
+INF = 0xffffffffffffffff
+def max(a,b);              a > b ? a : b                              end
+def min(a,b);              a < b ?  a : b                             end
+def swap(a,b);             a, b = b, a                                end
+def gif;                   gets.to_i                                  end
+def gff;                   gets.to_f                                  end
+def gsf;                   gets.chomp                                 end
+def gi;                    gets.split.map(&:to_i)                     end
+def gf;                    gets.split.map(&:to_f)                     end
+def gs;                    gets.chomp.split.map(&:to_s)               end
+def gc;                    gets.chomp.split('')                       end
+def pr(num);               num.prime_division                         end
+def pr?(num);              Prime.prime?(num)                          end
+def digit(num);            num.to_s.length                            end
+def array(s,ini=nil);      Array.new(s){ini}                          end
+def darray(s1,s2,ini=nil); Array.new(s1){Array.new(s2){ini}}          end
+def rep(num);              num.times{|i|yield(i)}                     end
+def repl(st,en,n=1);       st.step(en,n){|i|yield(i)}                 end
+
+e = gi
+b = gif
+l = gi
+jd = l.count(b) == 1
+ct = 0
+rep 6 do |i|
+  ct += 1 if l.count(e[i]) == 1
+end
+
+puts case
+when ct < 3 then 0
+when ct == 3 then 5
+when ct == 4 then 4
+when ct == 5 && jd then 2
+when ct == 5 then 3
+when ct == 6 then 1
+end
