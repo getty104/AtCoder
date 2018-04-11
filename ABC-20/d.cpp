ebaf48fe739036fe25d7eb7f151f30c4b744a438
getty104 <hayabusatoshihumi@gmail.com>
2018-04-11 10:26:58 +0900

    upda

--- ABC-20/d.cpp
+++ ABC-20/d.cpp
@@ -30,18 +30,42 @@ typedef priority_queue<P, vector<P>, greater<P>> PQ;
 #define repl(i,a,b)    for(ll i=(ll)(a);i<=(ll)(b);i++)
 #define dvec(n1,n2,i)  DVec(n1,Vec(n2,i))
 
-ll gcd(ll a, ll b){
-  if(a < b)swap(a,b);
-  ll r = a % b;
-  if(r == 0)return b;
-  return gcd(b,r);
-}
+
 int main(){
   cin.sync_with_stdio(false);
-  ll N,K;
-  cin >> N >> K;
-  ll sum = 0;
-  repl(i,1,N)sum += (i*K)/gcd(i,K) % MOD;
-  cout << sum % MOD << endl;
-  return 0;
+  Vec G;
+  ll n,k;
+  cin >> n >> k;
+  for(ll i=1; i*i <= k; i++){
+    if(k % i == 0){
+      G.pb(i);
+      if(i*i != k)G.pb(k/i);
+    }
+  }
+
+  ll ans = 0;
+  rep(i,G.size()){
+    ll s = k/G[i];
+    ll N = n/G[i];
+    Vec P;
+    ll sum = 0;
+
+    for(ll j=2; j*j <= s; j++){
+      if(s%j == 0)P.pb(j);
+      while(s%j == 0)s /= j;
+    }
+
+    if(s > 1)P.pb(s);
+    ll M = P.size();
+    for(ll j=0; j< 1<<M; j++){
+      bool sign= bcnt(j) % 2;
+      ll t=1;
+      rep(l,M)if(j>>l&1)t *= P[l];
+      ll u = N/t;
+      ll v = u*(u+1) / 2 % MOD * t % MOD;
+      sum = (sum + (sign ? -v : v) + MOD) % MOD;
+    }
+    ans = (ans + sum * k % MOD) % MOD;
+  }
+  cout << ans << endl;
 }
