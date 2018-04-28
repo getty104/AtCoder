be1e0a56b11deeccec4abeea6cfe1a6176def877
getty104 <hayabusatoshihumi@gmail.com>
2018-04-28 22:42:51 +0900

    update

---
+++ ABC-05/d.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll,ll> P;
+typedef vector<ll> Vec;
+typedef vector<Vec> DVec;
+typedef vector<DVec> TVec;
+typedef priority_queue<P, vector<P>, greater<P>> PQ;
+
+#define fi                first
+#define se                second
+#define pb                push_back
+#define INF               LLONG_MAX/3
+#define MOD               1000000007
+#define bcnt              __builtin_popcount
+#define all(x)            (x).begin(),(x).end()
+#define srt(x)            sort(all(x))
+#define rvs(x)            reverse(all(x))
+#define uni(x)            x.erase(unique(all(x)),x.end())
+#define ub(x,y)           (upper_bound(all(x),y)-x.begin())
+#define lb(x,y)           (lower_bound(all(x),y)-x.begin())
+#define rep(i,n)          repl(i,0,n-1)
+#define mkp(x,y)          make_pair(x,y)
+#define mmax(x,y)         (x>y?x:y)
+#define mmin(x,y)         (x<y?x:y)
+#define maxch(x,y)        x=mmax(x,y)
+#define minch(x,y)        x=mmin(x,y)
+#define exist(x,y)        (find(all(x),y)!=x.end())
+#define each(itr,v)       for(auto itr:v)
+#define repl(i,a,b)       for(ll i=(ll)(a);i<=(ll)(b);i++)
+#define dvec(n1,n2,i)     DVec(n1,Vec(n2,i))
+#define tvec(n1,n2,n3,i)  TVec(n1,dvec(n2,n3,i))
+
+int main(){
+  cin.sync_with_stdio(false);
+  ll n;
+  cin >> n;
+  Vec ans(5000);
+  DVec d = dvec(100,100,0);
+  repl(i,1,n)repl(j,1,n)cin >> d[i][j];
+
+  rep(i,n+1)rep(j,n+1)d[i][j+1] += d[i][j];
+  rep(j,n+1)rep(i,n+1)d[i+1][j] += d[i][j];
+
+  repl(i,1,n+1)repl(j,1,n+1)repl(k,i,n+1)repl(l,j,n+1)
+    maxch(ans[(k-i+1)*(l-j+1)],d[k][l]-d[k][j-1]-d[i-1][l]+d[i-1][j-1]);
+
+  ll tmp=0;
+  rep(i,n*n){
+    maxch(tmp,ans[i]);
+    maxch(ans[i],tmp);
+  }
+
+  ll q;
+  cin >> q;
+  while(q--){
+    ll p; cin >> p;
+    cout << ans[p] << endl;
+  }
+  return 0;
+}
