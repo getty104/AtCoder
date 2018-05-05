0d41dc8369ad2823db9951e2e1a5795dff11d7be
getty104 <hayabusatoshihumi@gmail.com>
2018-05-05 12:20:18 +0900

    update

---
+++ ARC/53/b.rb
@@ -0,0 +1,54 @@
+require 'prime'
+require 'set'
+require 'tsort'
+include Math
+ALP = ('a'..'z').to_a
+INF = 0xffffffffffffffff
+def max(a,b);              a > b ? a : b                              end
+def min(a,b);              a < b ?  a : b                             end
+def swap(a,b);             a, b = b, a                                end
+def gif;                   gets.to_i                                  end
+def gff;                   gets.to_f                                  end
+def gsf;                   gets.chomp                                 end
+def gi;                    gets.split.map(&:to_i)                     end
+def gf;                    gets.split.map(&:to_f)                     end
+def gs;                    gets.chomp.split.map(&:to_s)               end
+def gc;                    gets.chomp.split('')                       end
+def pr(num);               num.prime_division                         end
+def pr?(num);              Prime.prime?(num)                          end
+def digit(num);            num.to_s.length                            end
+def array(s,ini=nil);      Array.new(s){ini}                          end
+def darray(s1,s2,ini=nil); Array.new(s1){Array.new(s2){ini}}          end
+def rep(num);              num.times{|i|yield(i)}                     end
+def repl(st,en,n=1);       st.step(en,n){|i|yield(i)}                 end
+
+s = gc
+even = 0
+odd = 0
+ALP.each do |alp|
+  cnt = s.count(alp)
+  even += cnt / 2
+  odd += cnt % 2
+end
+
+ans = 0
+if odd > 0
+  ans = 1
+  loop() do
+    flag = true
+    even -= odd
+    if even < 0
+      flag = false
+      break
+    end
+    if flag
+      ans += 2
+    else
+      break
+    end
+  end
+else
+  ans = even * 2
+end
+
+puts ans
