4c9c13e81196c892255c67d5bcc1104b57b51e75
getty104 <hayabusatoshihumi@gmail.com>
2018-05-10 22:53:11 +0900

    update

---
+++ ARC/50/b.rb
@@ -0,0 +1,41 @@
+require 'prime'
+require 'set'
+require 'tsort'
+include Math
+ALP = ('a'..'z').to_a
+INF = 0xffffffffffffffff
+def max(a,b);              a > b ? a : b                              end
+def min(a,b);              a < b ?  a : b                             end
+def swap(a,b);             a, b = b, a                                end
+def gif;                   gets.to_i                                  end
+def gff;                   gets.to_f                                  end
+def gsf;                   gets.chomp                                 end
+def gi;                    gets.split.map(&:to_i)                     end
+def gf;                    gets.split.map(&:to_f)                     end
+def gs;                    gets.chomp.split.map(&:to_s)               end
+def gc;                    gets.chomp.split('')                       end
+def pr(num);               num.prime_division                         end
+def pr?(num);              Prime.prime?(num)                          end
+def digit(num);            num.to_s.length                            end
+def array(s,ini=nil);      Array.new(s){ini}                          end
+def darray(s1,s2,ini=nil); Array.new(s1){Array.new(s2){ini}}          end
+def rep(num);              num.times{|i|yield(i)}                     end
+def repl(st,en,n=1);       st.step(en,n){|i|yield(i)}                 end
+
+def f(k)
+($r - k >= 0) && ($b - k >= 0) && (($r-k)/($x-1)+($b-k)/($y-1) >= k)
+end
+
+$r,$b=gi
+$x,$y=gi
+l=0
+r=10**18
+while r-l > 1
+  md = (r+l)/2
+  if f(md)
+    l=md
+  else
+    r=md
+  end
+end
+puts l
